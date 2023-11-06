f762083416b14eab6e2dfd6e2d142c2f29b541d4
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-06 14:32:19 -0300

    Update questao8.c

--- questao8.c
+++ questao8.c
@@ -1,6 +1,6 @@
-//Exercício 8:
-//Desenvolva um programa que solicite ao usuário um número inteiro positivo e faça uma contagem regressiva a partir desse número até zero.
-// Utilize o switch case para exibir mensagens personalizadas para cada número da contagem regressiva.
+\\Exercício 8:
+\\Desenvolva um programa que solicite ao usuário um número inteiro positivo e faça uma contagem regressiva a partir desse número até zero.
+\\ Utilize o switch case para exibir mensagens personalizadas para cada número da contagem regressiva.
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
