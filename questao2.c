42d1f6726a8bc90a03b35f680ee9f97fe9b9edc7
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-06 14:43:23 -0300

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -1,87 +1,89 @@
-//Exercício 2: 
-//Você é um desenvolvedor de e-commerce e está trabalhando em um novo sistema de descontos. 
-//O sistema oferece um desconto de 10% para compras acima de R$ 100,00 se a compra for feita em um dia de semana.
-//Se a compra for feita em um fim de semana, o desconto será de 15% sobre o valor da compra acima de R$ 100,00.
+// Exercício 2:
+// Você é um desenvolvedor de e-commerce e está trabalhando em um novo sistema de descontos.
+// O sistema oferece um desconto de 10% para compras acima de R$ 100,00 se a compra for feita em um dia de semana.
+// Se a compra for feita em um fim de semana, o desconto será de 15% sobre o valor da compra acima de R$ 100,00.
 #include <stdio.h>
 #include <conio.h>
 #include <locale.h>
-int main(void)
+int main()
 {
-   int valorproduto;
-   char diadasemana[200];
-   float desconto;
-   setlocale(LC_ALL, "portuguese");
-   printf("Digite o valor do produto:");
-   scanf("%d", &valorproduto);
-   printf("Digite o dia da semana(1 á 7)");
-   switch (valorproduto)
-   {
-   case 1:
-      printf("Segunda-Feira\n");
-      if (valorproduto > 100)
-      {
-      }
-      else
-      {
-         desconto=(valorproduto * 0.10);
-         break;
-      case 2:
-         printf("Terça-Feira\n");
-         if (valorproduto > 100)
-         {
-         }
-         else
-         {
-            desconto=(valorproduto * 0.10);
-            break;
-         case 3:
-            printf("Quarta-feira\n");
-            if (valorproduto > 100)
-            {
-            }
-            else
-            {
-               desconto=(valorproduto * 0.10);
-               break;
-            case 4:
-               printf("Quinta-feira\n");
-               if (valorproduto > 100)
-               {
-               }
-               else
-               {
-                  desconto=(valorproduto * 0.10);
-                  break;
-               case 5:
-                  printf("Sexta-feira\n");
-                  if (valorproduto > 100)
-                  {
-                  }
-                  else
-                  {
-                     desconto=(valorproduto * 0.10);
-                     break;
-                  case 6:
-                     printf("Sábado\n");
-                     if (valorproduto > 100)
-                     {
-                     }
-                     else
-                     {
-                        desconto=(valorproduto * 0.15);
-                        break;
-                     case 7:
-                        printf("Domingo\n");
-                        if (valorproduto > 100)
-                        {
-                        }
-                        else
-                        {
-                           desconto=(valorproduto * 0.15);
-                           break;
-                           default:
-                           printf("Opção Inválida\n");
-                           printf("valorproduto:%d",valorproduto);
+    float valorproduto;
+    char diadasemana[200];
+    float desconto;
+    float valorFinal;
+    int opcao;
 
-                           return 0;
-                        }
+    setlocale(LC_ALL, "portuguese");
+
+    printf("Digite o valor do produto:");
+    scanf("%f", &valorproduto);
+
+    printf("Digite o dia da semana(1 á 7)");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        printf("Segunda-Feira\n");
+        if (valorproduto > 100)
+        {
+            desconto = (valorproduto * 0.10);
+        }
+
+        break;
+    case 2:
+        printf("Terça-Feira\n");
+        if (valorproduto > 100)
+        {
+            desconto = (valorproduto * 0.10);
+        }
+        break;
+    case 3:
+        printf("Quarta-feira\n");
+        if (valorproduto > 100)
+        {
+            desconto = (valorproduto * 0.10);
+        }
+
+        break;
+    case 4:
+        printf("Quinta-feira\n");
+        if (valorproduto > 100)
+        {
+            desconto = (valorproduto * 0.10);
+        }
+        break;
+    case 5:
+        printf("Sexta-feira\n");
+        if (valorproduto > 100)
+        {
+            desconto = (valorproduto * 0.10);
+        }
+        break;
+    case 6:
+        printf("Sábado\n");
+        if (valorproduto > 100)
+        {
+            desconto = (valorproduto * 0.15);
+        }
+        break;
+    case 7:
+        printf("Domingo\n");
+        if (valorproduto > 100)
+        {
+            desconto = (valorproduto * 0.15);
+        }
+        break;
+    default:
+        printf("Opção Inválida\n");
+    }
+
+   valorFinal = valorproduto - desconto;
+    
+    printf("valor produto:%f\n", valorproduto);
+    printf("valor Final %f \n", valorFinal);
+
+
+
+    return 0;
+}
