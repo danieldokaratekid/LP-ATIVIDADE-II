30dd6aacff3f1f837e929078b12c3854cb6620e2
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-06 14:31:22 -0300

    Update questao4.c

--- questao4.c
+++ questao4.c
@@ -1,6 +1,6 @@
 \\Exercício 4:
 \\Em um projeto de controle de acesso, crie um programa que solicite ao usuário sua idade e verifique se ele tem permissão para acessar um determinado recurso.
-\\ Se a idade for maior ou igual a 18, exiba a mensagem "Acesso permitido". Caso contrário, exiba a mensagem "Acesso negado".
+\\Se a idade for maior ou igual a 18, exiba a mensagem "Acesso permitido". Caso contrário, exiba a mensagem "Acesso negado".
 #include <stdio.h>
 #include <stdlib.h>
 int main(void)
